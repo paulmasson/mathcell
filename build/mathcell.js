0dca927dbaf2e32ce0ba8ad05567db071b728381
Paul Masson <paulmasson@analyticphysics.com>
2026-09-12 02:41:13 -0700

    Update injectFunctions for Safari

--- build/mathcell.js
+++ build/mathcell.js
@@ -434,14 +434,20 @@ function evaluate( id, data, config ) {
 
 function injectFunctions( id, functions, n='' ) {
 
+  function setFunctions() {
+    Object.keys( functions ).forEach( k => cw[k] = functions[k] );
+  }
+
   var output = document.getElementById( id + 'output' + n );
 
-  if ( output.children.length > 0 && output.children[0].contentWindow ) {
+  var iframe = output.children[0];
+  if ( !iframe ) throw Error( 'injectFuctions must follow evaluate for threejs output' );
 
-    var cw = output.children[0].contentWindow;
-    Object.keys( functions ).forEach( k => cw[k] = functions[k] );
+  var cw = iframe.contentWindow;
 
-  } else throw Error( 'injectFuctions must follow evaluate' );
+  if ( navigator.userAgent.includes( 'Safari' ) )
+    setTimeout( setFunctions, 50 );
+  else setFunctions();
 
 }
 
